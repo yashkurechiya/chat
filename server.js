47aa63d6cf258e62876a8f1c425363f70ff1d250
yashkurechiya <2005yashkurechiya@gmail.com>
2025-09-27 11:23:00 +0530

    Initi

--- server.js
+++ server.js
@@ -30,7 +30,8 @@ const pool = mysql.createPool({
     database: process.env.DB_NAME,
     waitForConnections: true,
     connectionLimit: 10,
-    queueLimit: 0
+    queueLimit: 0,
+    ssl: { rejectUnauthorized: true } 
 });
 
 console.log("Attempting to connect to database...");
